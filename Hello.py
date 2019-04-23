58451e7008a2ddc0883826440df2e8dafc88bb0c
The Crystal King <dylanawhite11@gmail.com>
2019-04-23 01:57:42 -0500

    created Hello.py

---
+++ Hello.py
@@ -0,0 +1,20 @@
+# This program says hello and asks for my name.
+
+print('Hello World!')
+
+# Ask for their name
+print('What is your name?')
+
+# Store user input in variable
+myName = input()
+
+# Display user input, check length of string
+print("It's good to meet you, " + myName)
+
+print('The length of your name is:')
+print(len(myName)
+
+# Ask for age, store user input in myAge variable
+print('What is your age?')
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year.")
